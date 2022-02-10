8f39dd5436784f67d58b9a3d20b844a6d238f029
Justin Maronde <justin.maronde@cengage.com>
2022-02-10 15:05:48 -0500

    Basic elements and interactions needed for the onboarding screen. Currently the get started button just uses a tapGesture as oppose to a slide which we will do.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -11,16 +11,101 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                } //: HEADER
+                
+                // MARK: - CENTER
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //: ZStack
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                } //: Center
+                
+                Spacer()
+                
+                // MARK: - FOOTER
+                ZStack {
+                    //Parts of the custom button
+                    
+                    //1. Background (static)
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    //2. Call-to-action (static)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    //3. Capsule shape (dynamic width)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    //4. Dragable Circle
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //: HStack
+                    
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
         
     }
 }
