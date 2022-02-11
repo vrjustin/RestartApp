8edd1541bf110729c81c5dc9144ad905f02bab52
Justin Maronde <justin.maronde@cengage.com>
2022-02-11 06:41:37 -0500

    basic overall design for the homeview.

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -12,14 +12,38 @@ struct HomeView: View {
 
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            //MARK - : Header
+            Spacer()
+            
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            //MARK - : CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            //MARK - : FOOTER
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            } //: Button
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VStack
     }
 }
