4ba6f705e012d22e8d89793a54dc876a9c5e2917
Justin Maronde <justin.maronde@cengage.com>
2022-02-11 07:21:54 -0500

    Make some state vars that when modified will re-render the screen. Change the tapGesture to a drag gesture and monitor the changes of the drag. Make the dragle circle offset based on where user has dragged to. on the end of the drag - determine where they stopped dragging - if not half way snap back else set the offset to the end of the draggable area and change our isOnboardingViewActive to change the state in the parent contentView and that will change the screen we are on.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     
     var body: some View {
         ZStack {
@@ -70,7 +74,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -87,15 +91,29 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if (gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80) {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                })
+                        ) //: Gesture
                         
                         Spacer()
                     } //: HStack
                     
                 } //: Footer
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
@@ -103,8 +121,11 @@ struct OnboardingView: View {
     }
 }
 
+// MARK: - PREVIEW
+
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
+.previewInterfaceOrientation(.portrait)
     }
 }
